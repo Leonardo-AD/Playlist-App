ae0e21035f33348c6d33257e23f591d92310f55f
Leonardo Alves <leonardo.alves779@gmail.com>
2023-02-24 17:03:52 -0300

    uploading files

---
+++ data.js
@@ -0,0 +1,24 @@
+export default [
+
+	{
+		title: "Heroes tonight",
+		artist: "Janji feat. Johnning",
+		cover: "heroes.jpg",
+		file: "Janji - Heroes Tonight (feat. Johnning) [NCS Release].mp3"
+	},
+	
+	{
+		title: "Lullaby",
+		artist: "R3hab feat. Mike Williams",
+		cover: "lullaby.jpg",
+		file: "R3HAB x Mike Williams - Lullaby (Official Video).mp3"
+	},
+
+	{
+		title: "Lovers on the sun",
+		artist: "David Guetta feat. Sam Martin",
+		cover: "lovers.jpg",
+		file: "David Guetta - Lovers On The Sun (Official Video) ft Sam Martin.mp3"
+	}
+
+];
